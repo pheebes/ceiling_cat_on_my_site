34eeb7423100e2a075055c415937d9a96737471b
Pheebes <phoebe.hardwicke@waterfield.com>
2015-12-16 09:40:43 -0600

    Added example seed data and took out some unnecessary styles.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,17 @@
 class WelcomeController < ApplicationController
+	before_action :set_reports
+	before_action :set_algorithms
 
 	def index
 		render :layout => true
+	end
+
+	private
+	def set_reports
+		@reports = Report.all
+	end
+
+	def set_algorithms
+		@algorithms = Algorithm.all
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+report1 = Report.create(author: "anonymous1", title: "Example 1", body: "This is the first example report.")
+report2 = Report.create(author: "anonymous2", title: "Example 2", body: "This is the second example report.")
+report3 = Report.create(author: "anonymous3", title: "Example 3", body: "This is the third example report.")
+
+algo1 = Algorithm.create(name: "example1", category: "technical", description: "Follow the institutional investors.")
+algo2 = Algorithm.create(name: "example2", category: "momentum", description: "This is example description two.")
+algo3 = Algorithm.create(name: "example3", category: "fundamental", description: "This is based on fundamental indicators, drivers, and trends.")
