1f45f55bdf4a18bd07510db288dac01729cf200e
Pheebes <phoebe.hardwicke@waterfield.com>
2015-12-16 20:04:50 -0600

    Problem was had not migrated production database... have to execute: heroku run rake db:migrate in order to migrate a heroku database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = true #set to true for debuggin production otherwise keep false
+  config.consider_all_requests_local       = false #set to true for debuggin production otherwise keep false
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
